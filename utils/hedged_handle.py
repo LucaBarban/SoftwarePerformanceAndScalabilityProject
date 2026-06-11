6852d2a9177a70a1b75fd7fa0874a6fe8de018d8
Tommaso Soncin <soncintommaso@gmail.com>
2026-06-11 21:12:32 +0200

    feat: hedged handler

--- utils/hedged_handle.py
+++ utils/hedged_handle.py
@@ -7,13 +7,14 @@ from .job import Job
 
 
 class HedgedHandle:
-    def __init__(self, id: int):
+    def __init__(self, id: int, completed: Queue):
         self.queue = Queue()
         self.id = id
         self.timing = Value("d", 0.0)
         self.processing_time = Value("d", 0.0)
-
-        self.server = HedgedServer(id, self.queue, self.timing, self.processing_time)
+        self.server = HedgedServer(
+            id, self.queue, completed, self.timing, self.processing_time
+        )
         self.server.start()
 
     def dispatch(self, job: Optional[Job]):
@@ -21,6 +22,10 @@ class HedgedHandle:
 
     def pendings(self):
         return self.queue.qsize()
+
+    @property
+    def pid(self) -> int:
+        return self.server.pid
 
     def current_age(self):
         if self.timing.value == 0.0:
